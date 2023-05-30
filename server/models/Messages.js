34f687d0f3b178f21c4a2ca1f337b2db8119836e
asgerbirk <asger.birk@gmail.com>
2023-05-30 18:37:15 +0200

    first

--- server/models/Messages.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const MessageSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    message: {type: String, required: true},
-});
-
-export const MessageModel = mongoose.model("messages", MessageSchema)
